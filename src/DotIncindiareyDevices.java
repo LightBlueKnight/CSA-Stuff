1f9a1640a3386caea609e63dfbc33c954a98af11
LightBlueKnight <lightningminecraft75@gmail.com>
2026-01-19 18:18:47 -0700

    finished exploding dots lab

--- src/DotIncindiareyDevices.java
+++ src/DotIncindiareyDevices.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import APClasses.IBIO;
+
 public class DotIncindiareyDevices {
-    
+    public DotIncindiareyDevices(){
+
+    }
+
+    public static ArrayList<Integer> explodeDots(int num, int base){
+        boolean cancel = false;
+        if(base < 2) cancel = true;
+        if(num < 0) cancel = true;
+        
+        ArrayList<Integer> explodedDots = new ArrayList<Integer>();
+        if(cancel) return explodedDots;
+
+
+        while(num>0){
+            explodedDots.add(subtractModulus(num,base));
+            num/=base;
+        }
+        return explodedDots;
+    }
+
+    private static int subtractModulus(int num, int base){
+        while(true){
+            if(num>-1 && num < base){
+                return num;
+            }
+            num -= base;
+        }
+    }
+    public static void main(String[] args) {
+        IBIO console = new IBIO();
+        console.output((DotIncindiareyDevices.explodeDots(314, 10)).toString());
+    }
 }
